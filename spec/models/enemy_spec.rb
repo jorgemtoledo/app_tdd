87a78c2d6d2880b8c13fda3b7768d5508496943a
jorgemtoledo <jorge.toledo@gmail.com>
2020-07-25 17:06:50 -0300

    Add testes weapon

--- spec/models/enemy_spec.rb
+++ spec/models/enemy_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
+require 'ffaker'
 
 RSpec.describe Enemy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:enemy) { create(:enemy) }
+
+  it 'returns the correct current power' do
+    expect(enemy.current_power).to eq(enemy.power_base + ((enemy.level - 1) * enemy.power_step))
+  end
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -9,26 +9,14 @@ RSpec.describe Weapon, type: :model do
     expect(weapon).to_not be_valid
   end
 
-  # it 'is invalid if the power_step is not equal 100' do
-  #   weapon1 = build(:weapon, power_step: Random.rand(0..99))
-  #   weapon2 = build(:weapon, power_step: Random.rand(101..9999))
-  #   expect(weapon1).to_not be_valid
-  #   expect(weapon2).to_not be_valid
-  # end
-
-  it 'is invalid if the power_base is not greather than or equal to 3000' do
-    weapon = build(:weapon, power_base: Random.rand(0..2999))
-    expect(weapon).to_not be_valid
+  it "returns the weapon current power" do
+    weapon = create(:weapon)
+    power_weapon = (weapon.power_base + ((weapon.level - 1) * weapon.power_step))
+    expect(weapon.current_power).to eq(power_weapon)
   end
 
-  # it "returns the weapon current power" do
-  #   weapon = create(:weapon)
-  #   power_weapon = (weapon.power_base + ((weapon.level - 1) * weapon.power_step))
-  #   expect(weapon.current_power).to eq(power_weapon)
-  # end
-
-  # it "returns the correct weapon title" do
-  #   weapon = create(:weapon)
-  #   expect(weapon.title).to eq("#{weapon.name} ##{weapon.level}")
-  # end
+  it "returns the correct weapon title" do
+    weapon = create(:weapon)
+    expect(weapon.title).to eq("#{weapon.name} ##{weapon.level}")
+  end
 end
